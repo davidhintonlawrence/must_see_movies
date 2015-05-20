278b3d95a8bbcc0745fc7e6b6b2f51a4c55a7bac
davidhintonlawrence <dlawrence2016@kellogg.northwestern.edu>
2015-05-20 12:35:05 -0500

    d

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,17 @@
 class Role < ActiveRecord::Base
+
+  belongs_to :actor,:class_name => "Actor", :foreign_key => "actor_id"
+  # name for method, table to search
+
+# # equivalent to:
+# def actor
+#   return Actor.find_by({ :id => self.actor_id})
+# end
+
+
+
+# Add the belongs_to for movie. Use the .actor and .movie methods in roles#index and roles# show
+
+belongs_to :movie, :class_name => "Movie", :foreign_key => "movie_id"
+
 end
